ef780a70ebed9249e211dd5eedc55cc6519bab78
festus-sulumeti <sulumetifestus@outlook.com>
2025-10-18 11:54:06 +0300

    feat: The about init

---
+++ src/components/Herosection/About.tsx
@@ -0,0 +1,143 @@
+'use client'
+import { Cpu, Lock, Sparkles, Zap } from 'lucide-react'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { Menu, X } from 'lucide-react'
+import About from "../../assets/5.png"
+
+
+import { Link } from 'react-router-dom'
+
+import Footer from './Footer'
+import { ModeToggle } from '../mode-toggle'
+
+const menuItems = [
+    { name: 'Home', to: '/' },
+    { name: 'Features', to: '#' },
+    { name: 'Solution', to: '#' },
+    { name: 'Pricing', to: '#' },
+    { name: 'About', to: '/about' },
+]
+
+
+export default function ContentSection() {
+    const [menuState, setMenuState] = useState(false)
+
+    return (
+        <>
+        <header>
+                <nav
+                    data-state={menuState && 'active'}
+                    className="fixed z-20 w-full border-b border-dashed bg-white backdrop-blur md:relative dark:bg-zinc-950/50 lg:dark:bg-transparent">
+                    <div className="m-auto max-w-5xl px-6">
+                        <div className="flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
+                            <div className="flex w-full justify-between lg:w-auto">
+                                <Link
+                                    to="/"
+                                    aria-label="home"
+                                    className="flex items-center space-x-2">
+                                    {/* <Logo /> */}
+                                </Link>
+
+                                <button
+                                    onClick={() => setMenuState(!menuState)}
+                                    aria-label={menuState == true ? 'Close Menu' : 'Open Menu'}
+                                    className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden">
+                                    <Menu className="in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
+                                    <X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
+                                </button>
+                            </div>
+
+                            <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
+                                <div className="lg:pr-4">
+                                    <ul className="space-y-6 text-base lg:flex lg:gap-8 lg:space-y-0 lg:text-sm">
+                                        {menuItems.map((item, index) => (
+                                            <li key={index}>
+                                                <Link
+                                                    to={item.to}
+                                                    className="text-muted-foreground hover:text-accent-foreground block duration-150">
+                                                    <span>{item.name}</span>
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+
+                                <ModeToggle />
+
+                                <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit lg:border-l lg:pl-6">
+                                    <Button
+                                        asChild
+                                        variant="outline"
+                                        size="sm">
+                                        <Link to="/login">
+                                            <span>Login</span>
+                                        </Link>
+                                    </Button>
+
+                                    <Button
+                                        asChild
+                                        size="sm">
+                                        <Link to="/signup">
+                                            <span>Get Started</span>
+                                        </Link>
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+
+            <section className="py-16 md:py-32">
+            <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-12">
+                <div className="mx-auto max-w-xl space-y-6 text-center md:space-y-12">
+                    <h2 className="text-balance text-4xl font-medium lg:text-5xl"> Sacco Yetu — powering Africa’s digital SACCO revolution.</h2>
+                    <p>Sacco Yetu transforms how communities, small businesses, and cooperatives save and borrow. 
+                        It’s a digital SACCO platform that brings trust, speed, and transparency to everyday 
+                        finance — enabling members to contribute savings, request loans, and track repayments
+                        all in one secure, mobile-first experience. Designed for Africa, built for growth.
+                    </p>
+                </div>
+                {/* <img className="rounded-(--radius) grayscale" src="https://images.unsplash.com/photo-1616587226960-4a03badbe8bf?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="team image" height="" width="" loading="lazy" /> */}
+                 <img className="rounded-(--radius) grayscale" src={About} alt="team image" height="" width="" loading="lazy" />
+
+                <div className="relative mx-auto grid grid-cols-2 gap-x-3 gap-y-6 sm:gap-8 lg:grid-cols-4">
+                    <div className="space-y-3">
+                        <div className="flex items-center gap-2">
+                            <Zap className="size-4" />
+                            <h3 className="text-sm font-medium">Faaast</h3>
+                        </div>
+                        <p className="text-muted-foreground text-sm">It supports an entire helping developers and innovate.</p>
+                    </div>
+                    <div className="space-y-2">
+                        <div className="flex items-center gap-2">
+                            <Cpu className="size-4" />
+                            <h3 className="text-sm font-medium">Powerful</h3>
+                        </div>
+                        <p className="text-muted-foreground text-sm">It supports an entire helping developers and businesses.</p>
+                    </div>
+                    <div className="space-y-2">
+                        <div className="flex items-center gap-2">
+                            <Lock className="size-4" />
+                            <h3 className="text-sm font-medium">Security</h3>
+                        </div>
+                        <p className="text-muted-foreground text-sm">It supports an helping developers businesses innovate.</p>
+                    </div>
+                    <div className="space-y-2">
+                        <div className="flex items-center gap-2">
+                            <Sparkles className="size-4" />
+
+                            <h3 className="text-sm font-medium">AI Powered</h3>
+                        </div>
+                        <p className="text-muted-foreground text-sm">It supports an helping developers businesses innovate.</p>
+                    </div>
+                </div>
+            </div>
+            </section>
+
+            <Footer />
+        </>
+        
+    )
+}
