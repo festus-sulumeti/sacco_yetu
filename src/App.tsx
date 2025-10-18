dc73a9f7efa76ef5b0bb22bad5c3124bb2146c79
festus-sulumeti <sulumetifestus@outlook.com>
2025-10-18 12:16:01 +0300

    feat: project

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
 import HeroSection from './components/Herosection/HeroSection'
-import { ThemeProvider } from "@/components/theme-provider"
 import SignupPage from './Pages/Signup'
-
+import LoginPage from './Pages/Login'
+import About from './components/Herosection/About'
+import Dashboard from './components/Dashboard/Dasboard'
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   
 
   return (
     <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path='/login' element={<LoginPage />} /> 
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
 
-       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <Routes>
-            <Route path="/" element={<HeroSection />} />
-            <Route path="/signup" element={<SignupPage />} />
-            
-          </Routes>
-      </ThemeProvider>
+    </ThemeProvider>
      
     </>
   )
