b6b768ab2ec507b9e625d562d4ac85b9bbf483ae
festus-sulumeti <sulumetifestus@outlook.com>
2025-10-18 11:48:54 +0300

    feat: FAQ import init

--- src/components/Herosection/HeroSection.tsx
+++ src/components/Herosection/HeroSection.tsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import DetailsSector from './Detailssection'
 import SectionPage from './Section'
+import FAQ from './FAQ'
 
 
 
@@ -163,6 +164,7 @@ export default function HeroSection() {
 
             <DetailsSector />
             <SectionPage />
+            <FAQ />
         </>
     )
 }
